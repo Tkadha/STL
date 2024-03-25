5d9dd901b260cfbb737f3e9e234ad494759f329a
Tkadha <84236232+Tkadha@users.noreply.github.com>
2024-03-25 15:18:36 +0900

    4주차 1일

--- 수업내용/FileName.cpp
+++ 수업내용/FileName.cpp
@@ -1,22 +1,38 @@
 //---------------------------------------------------------------------
-// 2024.3.18 월		                                        (3주 / 1일)
+// 2024.3.25 월		                                        (4주 / 1일)
 // 
-// 실행 파일의 메모리 영역 - STACK, DATA, CODE, free store(heap)
+// free store에 할당된 객체
+// 
+// 앞으로 C++에서 사용하지 말 것
+// raw        wrapping
+// char*   -> string
+// T [N]   -> array<T,N>
+// T*      -> unique_ptr, shared_ptr
 //---------------------------------------------------------------------
 
 #include <iostream>
-#include <array>
-#include <string>
+#include <string_view>
+#include <fstream>
+#include <vector>
 #include "save.h"
 using namespace std;
 
-char gc;	
-int gnum;
+// [문제] "FileName.cpp"의 모든 영문자를 대문자로 바꿔 "FileName대문자.txt"에 저장하시오.
 
 int main()
 {
-	array<int, 10> a;
-	string s{ "안녕하세요" };
-	double* p = new double[10];
+	string_view name{ "FileName.cpp" };
+	ifstream in{ name.data() };
+	if (!in) {
+		std::cout << "파일을 열지 못함" << '\n';
+		exit(-1);
+	}
+	std::ofstream out("FileNam대문자.txt");
+	
+	char c;
+	in >> noskipws;
+	while (in >> c) {
+		out << static_cast<char>(toupper(c));
+	}
 	save("FileName.cpp");
 }
